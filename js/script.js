caac96412cc139e456805a56c2482561d1bf00ff
arthuramosantos <arthuramossan@gmail.com>
2024-05-28 08:00:04 -0300

    commmita

--- js/script.js
+++ js/script.js
@@ -1,26 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
     alert("Olá, sou o Irock, seu Bicho de Estimação Favorito");
 
-    const ROCK_IMAGEM_SRC = "./img/IROCK.joinha.png";
+    const ROCK_IMAGEM_SRC = "./img/S.W.png";
+    const NOVA_IMAGEM_FUNDO_SRC = "./img/bar.jpg";
+
     const rockImage = document.getElementById('ROCK');
+    const imagemFundo = new Image();
 
     rockImage.addEventListener('click', mudarDialogo);
 
     function mudarDialogo() {
         let userName = prompt("Qual é o seu nome?");
         if (userName) {
-            alert("Olá, " + userName + "! Prazer em te conhecer!");
+            alert("Olá, " + userName + "! Vamo tomar uma!");
             trocarImagem();
         }
     }
 
     function trocarImagem() {
+        // Trocar a imagem do bicho de estimação
         rockImage.src = ROCK_IMAGEM_SRC;
+
+        // Quando a nova imagem de fundo for carregada, trocar o fundo do site
+        imagemFundo.onload = function() {
+            document.body.style.backgroundImage = 'url(' + NOVA_IMAGEM_FUNDO_SRC + ')';
+        };
+
+        // Pré-carregar a nova imagem de fundo
+        imagemFundo.src = NOVA_IMAGEM_FUNDO_SRC;
     }
 });
+
 ///isso seria oq o livro esta escrito
 //fuction touchRock(){var userName = prompt("Qual o seu nome?",
-//                                   "Entre com seu nome aqui."  
+//                                   "Entre com seu nome aqui."  ///////////                                   "Entre com seu nome aqui."  
 
 //if (userName){
 //  alert("Prazer em conhece você," + userName + ".");
